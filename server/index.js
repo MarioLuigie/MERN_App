47685515aaa6b679948d110155177ee20ac001ea
Mariusz Łotocki <mk.lotocki@gmail.com>
2023-12-05 14:14:28 +0100

    Create connecting to database with mongoose

--- server/index.js
+++ server/index.js
@@ -2,3 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://mklotocki86:September1939@mern.odw5adb.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(CONNECTION_URL);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
+connectToDatabase();
+
+
