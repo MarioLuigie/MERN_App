dee6a17a29c1a3c6d17218e84591d4d9485b9fc8
Mariusz Łotocki <mk.lotocki@gmail.com>
2023-12-08 12:45:23 +0100

    Create buttons and icons from Material UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import IconButton from '@mui/material/IconButton';
 
 export default function App() {
 
   return (
     <>
-      <p>APP START</p>
+      <Button startIcon={<SendIcon />} variant="contained" onClick={() => {console.log("Click")}}>Send</Button>
+
+      <IconButton aria-label="delete">
+        <DeleteIcon />
+      </IconButton>
+
+      <IconButton color="secondary" aria-label="add an alarm">
+  <AlarmIcon />
+</IconButton>
+
+<IconButton color="primary" aria-label="add to shopping cart">
+  <AddShoppingCartIcon />
+</IconButton>
     </>
   )
 }
