db202f9ef6d5b8b4815da8319e1544b2463bbe93
Mariusz Łotocki <mk.lotocki@gmail.com>
2023-12-20 12:39:22 +0100

    Fix problems with multer and upload files

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,26 +1,14 @@
 import express from "express";//express import
-import multer from "multer";
-import path from "path";
 
-import * as controllers from "../controllers/posts.js"
+import * as controllers from "../controllers/posts.js";
+import multerUpload from "../middlewares/multer.js";
 
 const router = express.Router();//create express Router instance/object for create routes
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "../uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-})
-
-const upload = multer({ storage });
 
 //GET request serving/handle
 router.get("/", controllers.getPosts);
 //POST request serving/handle
-router.post("/", upload.array("files", 4), controllers.createPost);
+router.post("/", multerUpload(), controllers.createPost);
 
 export default router;
 
