9ec859a6912813698030a43acd0c66e983eb330a
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-01-02 19:05:17 +0100

    Add cors origin headers to middlewares stack in index.js on server side

--- server/index.js
+++ server/index.js
@@ -9,6 +9,12 @@ const app = express();
 dotenv.config();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.json());
 app.use("/api/posts", postRoutes);
 
