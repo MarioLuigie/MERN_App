a8e0d2df2dfefa284ab47e5c21826942ec8ad300
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-01-02 16:30:25 +0100

    Fix code

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,7 +6,7 @@
     try {
       const postMessages = await PostMessage.find();
       res.status(200).json(postMessages);
-      console.log(postMessages);
+      console.log("Pobrane zasoby z mDB", postMessages);
     } catch (err) {
       res.status(404).json({ message: err.message});
       console.log(err.message);
