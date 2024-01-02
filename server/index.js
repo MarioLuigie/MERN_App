1595e87c6c815abc9998c8194069a62d657313b0
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-01-02 15:37:47 +0100

    Create localhost server

--- server/index.js
+++ server/index.js
@@ -18,9 +18,9 @@ const PORT = process.env.PORT;
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(CONNECTION_URL);
-    // app.listen(PORT, () => {
-    //   console.log(`Server is running on port ${PORT}`);
-    // });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
   }
