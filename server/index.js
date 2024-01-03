6d43fc81621b4cd5ca9641c115ba02a31868c7d7
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-01-03 11:45:29 +0100

    Fix code

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ import postRoutes from "./routes/posts.js";
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://mern-app-client-oufp.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 app.use("/api/posts", postRoutes);
 
