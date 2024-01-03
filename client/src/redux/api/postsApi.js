b094b0a15fee6ff30bc04321a02ff35270c0d42d
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-01-03 14:18:02 +0100

    Fix code

--- client/src/redux/api/postsApi.js
+++ client/src/redux/api/postsApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const url = import.meta.env.VITE_REACT_APP_API_URL;
-const url = "https://mern-app-client-oufp.vercel.app";
+const url = "https://mern-app-marioluigie-server.vercel.app/";
 const postsAxios = axios.create({baseURL: `${url}/api`});
 
 export const getPosts = () => {
