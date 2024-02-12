bb8a3dc20510dbbd556d272d2a946d6b10969e90
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-02-12 01:56:33 +0100

    Get info about the post in postDetails.jsx

--- client/src/components/content/PostDetails.jsx
+++ client/src/components/content/PostDetails.jsx
@@ -1,18 +1,32 @@
 // /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const styles = css`
   height: 600px;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `
 
 export default function PostDetails() {
 
+  const params = useParams();
+
+  const { postsList } = useSelector(store => store.posts);
+
+  const post = postsList?.find(post => post._id === params.id);
+
+  console.log("***:", post)
+
   return (
     <div css={styles}>
-      POST DETAILS
+      <p>{post?.name}</p>
+      <p>{post?.title}</p>
+      <p>{post?.message}</p>
+      <p>{post?.tags.map(tag => `${tag},`)}</p>
     </div>
   )
 }
